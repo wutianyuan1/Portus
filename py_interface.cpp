ff4c5352a7a5c5aa402d039702cddaf3b765f0dc
stevelee477 <hi.whoareyou12@gmail.com>
2022-11-08 23:25:12 +0800

    update docstring

--- py_interface.cpp
+++ py_interface.cpp
@@ -4,8 +4,9 @@
 #include "checkpoint.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("init_checkpoint", &init_checkpoint, "init checkpoint");
-    m.def("checkpoint", &checkpoint, "do checkpointing");
+    m.doc() = "fast DNN checkpoint with RPMA";
+    m.def("init_checkpoint", &init_checkpoint, "init checkpoint", py::arg("network_name"), py::arg("state_dict"), py::arg("ib_local_addr") = std::string("192.168.10.101"), py::arg("server_addr") = std::string("192.168.10.4"));
+    m.def("checkpoint", &checkpoint, "do checkpointing", py::arg("use_async") = true);
     m.def("restore", &restore, "do restore");
     m.def("wait_checkpoint_done", &wait_checkpoint_done, "wait checkpoint done");
 }
